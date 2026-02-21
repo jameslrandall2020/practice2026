aa4bd185a94c820865b06e60421bed861cc50e76
James Randall <jameslrandall2020@gmail.com>
2026-02-21 14:45:00 -0500

    real commit

---
+++ hello_mont.py
@@ -0,0 +1,8 @@
+username=input("Enter your name: ")
+print(f"Nice to meet you, {username}")
+numbers=int(input("Enter lottery numbers:"))
+print(f"The winning lucky number are {numbers}")
+
+age=int(input("What is your age: "))
+next_age=(age + 1)
+print(f"Your age is {age} and next year you will be {next_age}.")
