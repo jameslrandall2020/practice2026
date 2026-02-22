3c28c91d7563be3f44ed8914a5d1edb4bcbc90d6
James Randall <jameslrandall2020@gmail.com>
2026-02-21 23:02:39 -0500

    late night

---
+++ new_code.py
@@ -0,0 +1,37 @@
+char="#"
+print(f"{char*5}")
+print(f"{char}")
+print(f"{char}")
+print(f"{char}")
+print(f"{char*5}")
+
+
+print(f"{char*6}")
+print(f"{char}    {char}")
+print(f"{char}    {char}")
+print(f"{char}    {char}")
+print(f"{char}    {char}")
+print(f"{char*6}")
+
+
+print(f"{char*4}")
+print(f"{char}    {char}")
+print(f"{char}    {char}")
+print(f"{char}    {char}")
+print(f"{char}    {char}")
+print(f"{char*4}")
+
+
+print(f"{char*4}")
+print(f"{char}")
+print(f"{char*4}")
+print(f"{char}")
+print(f"{char*4}")
+
+print(f"{char*5}")
+print(f"{char}    {char}")
+print(f"{char}    {char}")
+print(f"{char*5}")
+print(f"{char}    {char}")
+print(f"{char}     {char}")
+print(f"{char}      {char}" )
